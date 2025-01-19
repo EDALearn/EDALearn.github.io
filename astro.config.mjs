2c553af073f89d50105943b7be2e5029ff2993cc
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-01-19 01:33:32 +0100

    adds new astro starlight website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://edalearn.github.io/',
+	outDir: 'dist',
 	integrations: [
 		starlight({
 			title: 'EDALearn',
+			favicon: '/favicon.png',
 			logo: {
 				src: './src/assets/event-driven-architectures-logo-transparent.png',
 			},
